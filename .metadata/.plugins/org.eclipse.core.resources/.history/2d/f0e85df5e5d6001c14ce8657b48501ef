77f7a483476271a1660f9091cbd1115f46b9cc53
ThomasTapia16 <thomastlsoftware@gmail.com>
2022-05-18 16:16:55 -0400

    vistas actualizadas

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2d/f0e85df5e5d6001c14ce8657b48501ef
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+		
+	@RequestMapping("/")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/crearColaborador")
+	public String crearColaborador()
+	{	
+		return "crearColaborador";
+	}
+	
+	 @GetMapping("/crearAdministrador")
+	    public String showNewCourseForm(Model model) {
+	        Course Course = new Course();
+	        model.addAttribute("course", Course);
+	        return "new_course";
+	    }
+}
--- ProyectoSmartFit/src/main/java/com/example/demo/controller/MainController.java
+++ ProyectoSmartFit/src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,8 +29,31 @@ public class MainController {
 	
 	 @GetMapping("/crearAdministrador")
 	    public String showNewCourseForm(Model model) {
-	        Course Course = new Course();
-	        model.addAttribute("course", Course);
-	        return "new_course";
+	        //Course Course = new Course();
+	        //model.addAttribute("course", Course);
+	        return "crearAdministrador";
 	    }
+	 @GetMapping("/crearSuperAdministrador")
+		public String crearSuperAdministrador()
+		{	
+			return "crearSuperAdministrador";
+		}
+	 @GetMapping("/crearSala")
+		public String crearSala()
+		{	
+			return "crearSala";
+		}
+	 @GetMapping("/crearSede")
+		public String crearSede()
+		{	
+			return "crearSede";
+		}
+	 @GetMapping("/cerrarSesion")
+		public String cerrarSesion()
+		{	
+			return "login";
+		}
+	 
+	 
+	 
 }
